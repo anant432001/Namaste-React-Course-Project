5d7248a30f54e408da4445a5e65d76c044627156
Anant Pandey <anant432001@gmail.com>
2023-10-02 15:37:58 +0530

    done

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,11 +1,97 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Contact = () => {
-    return(
-        <div>
-            This is Contact Page..
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const handleChange = (e) => {
+    // Event Object Destructuring:
+    // e is the event object passed to the function when an input field's value changes.
+    // e.target represents the DOM element that triggered the event, which in this case is an input field.
+    // name and value are extracted from the input field. The name attribute is typically used to identify which field is being updated.
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // setFormData();
+    setFormData(() => ({
+      name: "",
+      email: "",
+      message: "",
+    }));
+    // Add your form submission logic here
+    console.log("Form data submitted:", formData);
+  };
+
+  return (
+    <div className="max-w-md mx-auto mt-10 p-6 bg-white rounded-md shadow-md">
+      <h2 className="text-2xl font-semibold mb-6">Contact Us</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="mb-4">
+          <label
+            htmlFor="name"
+            className="block text-sm font-medium text-gray-600">
+            Name
+          </label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+            className="mt-1 p-2 w-full border rounded-md"
+            required
+          />
         </div>
-    );
-}
+        <div className="mb-4">
+          <label
+            htmlFor="email"
+            className="block text-sm font-medium text-gray-600">
+            Email
+          </label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            className="mt-1 p-2 w-full border rounded-md"
+            required
+          />
+        </div>
+        <div className="mb-6">
+          <label
+            htmlFor="message"
+            className="block text-sm font-medium text-gray-600">
+            Message
+          </label>
+          <textarea
+            id="message"
+            name="message"
+            rows="4"
+            value={formData.message}
+            onChange={handleChange}
+            className="mt-1 p-2 w-full border rounded-md"
+            required></textarea>
+        </div>
+        <div>
+          <button
+            type="submit"
+            className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600">
+            Submit
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
 
 export default Contact;
